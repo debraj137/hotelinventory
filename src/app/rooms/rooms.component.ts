13b2a2e33385a8b499039f4d6a615a8bc6b75f0a
Debraj Deb <debrajdeb137@gmail.com>
2025-02-16 15:02:26 +0530

    directives

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,21 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { Room, RoomList } from './rooms';
 
 @Component({
   selector: 'app-rooms',
   templateUrl: './rooms.component.html',
-  styleUrls: ['./rooms.component.scss']
+  styleUrls: ['./rooms.component.scss'],
 })
 export class RoomsComponent implements OnInit {
   hotelName = 'Hilton Hotel!';
   numberOfRooms = 10;
   hideRooms = false;
-  constructor() { }
+  rooms: Room = {
+    totalRooms: 20,
+    availableRooms: 10,
+    bookedRooms: 5,
+  };
 
-  ngOnInit(): void {
-  }
+  roomList:RoomList[] = [
+    {
+      roomNumber: 1,
+      roomType: 'Deluxe Room',
+      amenities: 'AC, Free-Wifi, Bathroom, Kitchen',
+      price: 500,
+      photos: 'https://media.istockphoto.com/id/1208955086/photo/door-opened-to-bedroom.jpg?s=1024x1024&w=is&k=20&c=6t4VanNXaVaGcZatu9P1dBSJG1fa1NHeft9yZiFkIIc=',
+      checkinTime: new Date('11-Nov-2021'),
+      checkoutTime: new Date('12-Nov-2021')
+    },
+    {
+      roomNumber: 2,
+      roomType: 'Deluxe Room',
+      amenities: 'AC, Free-Wifi, Bathroom, Kitchen',
+      price: 1000,
+      photos: 'https://media.istockphoto.com/id/1208955086/photo/door-opened-to-bedroom.jpg?s=1024x1024&w=is&k=20&c=6t4VanNXaVaGcZatu9P1dBSJG1fa1NHeft9yZiFkIIc=',
+      checkinTime: new Date('11-Nov-2021'),
+      checkoutTime: new Date('12-Nov-2021')
+    },
+    {
+      roomNumber: 3,
+      roomType: 'Private Suite',
+      amenities: 'AC, Free-Wifi, Bathroom, Kitchen',
+      price: 15000,
+      photos: 'https://media.istockphoto.com/id/1208955086/photo/door-opened-to-bedroom.jpg?s=1024x1024&w=is&k=20&c=6t4VanNXaVaGcZatu9P1dBSJG1fa1NHeft9yZiFkIIc=',
+      checkinTime: new Date('11-Nov-2021'),
+      checkoutTime: new Date('12-Nov-2021')
+    }
+  ]
+  constructor() {}
 
-  toggle(){
+  ngOnInit(): void {}
+
+  toggle() {
     this.hideRooms = !this.hideRooms;
   }
-
 }
