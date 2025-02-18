e045e0ab34e501e54564b28e03579b44e7b0095a
Debraj Deb <debrajdeb137@gmail.com>
2025-02-18 22:57:17 +0530

    component communication using input output

---
+++ src/app/rooms/rooms-list/rooms-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { RoomList } from '../rooms';
+// import { EventEmitter } from 'stream';
+
+@Component({
+  selector: 'app-rooms-list',
+  templateUrl: './rooms-list.component.html',
+  styleUrls: ['./rooms-list.component.scss']
+})
+export class RoomsListComponent implements OnInit {
+  @Input() rooms: RoomList[] = [];
+  @Output() selectedRoom = new EventEmitter<RoomList>();
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  selectRoom(room: RoomList){
+    this.selectedRoom.emit(room);
+  }
+}
--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -10,49 +10,58 @@ export class RoomsComponent implements OnInit {
   hotelName = 'Hilton Hotel!';
   numberOfRooms = 10;
   hideRooms = false;
+  selectedRoom!: RoomList
   rooms: Room = {
     totalRooms: 20,
     availableRooms: 10,
     bookedRooms: 5,
   };
 
-  roomList:RoomList[] = [
-    {
-      roomNumber: 1,
-      roomType: 'Deluxe Room',
-      amenities: 'AC, Free-Wifi, Bathroom, Kitchen',
-      price: 500,
-      photos: 'https://media.istockphoto.com/id/1208955086/photo/door-opened-to-bedroom.jpg?s=1024x1024&w=is&k=20&c=6t4VanNXaVaGcZatu9P1dBSJG1fa1NHeft9yZiFkIIc=',
-      checkinTime: new Date('11-Nov-2021'),
-      checkoutTime: new Date('12-Nov-2021'),
-      rating: 4.5
-    },
-    {
-      roomNumber: 2,
-      roomType: 'Deluxe Room',
-      amenities: 'AC, Free-Wifi, Bathroom, Kitchen',
-      price: 1000,
-      photos: 'https://media.istockphoto.com/id/1208955086/photo/door-opened-to-bedroom.jpg?s=1024x1024&w=is&k=20&c=6t4VanNXaVaGcZatu9P1dBSJG1fa1NHeft9yZiFkIIc=',
-      checkinTime: new Date('11-Nov-2021'),
-      checkoutTime: new Date('12-Nov-2021'),
-      rating: 3.4
-    },
-    {
-      roomNumber: 3,
-      roomType: 'Private Suite',
-      amenities: 'AC, Free-Wifi, Bathroom, Kitchen',
-      price: 15000,
-      photos: 'https://media.istockphoto.com/id/1208955086/photo/door-opened-to-bedroom.jpg?s=1024x1024&w=is&k=20&c=6t4VanNXaVaGcZatu9P1dBSJG1fa1NHeft9yZiFkIIc=',
-      checkinTime: new Date('11-Nov-2021'),
-      checkoutTime: new Date('12-Nov-2021'),
-      rating: 2.6
-    }
-  ]
+  roomList:RoomList[] = []
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.roomList = [
+      {
+        roomNumber: 1,
+        roomType: 'Deluxe Room',
+        amenities: 'AC, Free-Wifi, Bathroom, Kitchen',
+        price: 500,
+        photos: 'https://media.istockphoto.com/id/1208955086/photo/door-opened-to-bedroom.jpg?s=1024x1024&w=is&k=20&c=6t4VanNXaVaGcZatu9P1dBSJG1fa1NHeft9yZiFkIIc=',
+        checkinTime: new Date('11-Nov-2021'),
+        checkoutTime: new Date('12-Nov-2021'),
+        rating: 4.5
+      },
+      {
+        roomNumber: 2,
+        roomType: 'Deluxe Room',
+        amenities: 'AC, Free-Wifi, Bathroom, Kitchen',
+        price: 1000,
+        photos: 'https://media.istockphoto.com/id/1208955086/photo/door-opened-to-bedroom.jpg?s=1024x1024&w=is&k=20&c=6t4VanNXaVaGcZatu9P1dBSJG1fa1NHeft9yZiFkIIc=',
+        checkinTime: new Date('11-Nov-2021'),
+        checkoutTime: new Date('12-Nov-2021'),
+        rating: 3.4
+      },
+      {
+        roomNumber: 3,
+        roomType: 'Private Suite',
+        amenities: 'AC, Free-Wifi, Bathroom, Kitchen',
+        price: 15000,
+        photos: 'https://media.istockphoto.com/id/1208955086/photo/door-opened-to-bedroom.jpg?s=1024x1024&w=is&k=20&c=6t4VanNXaVaGcZatu9P1dBSJG1fa1NHeft9yZiFkIIc=',
+        checkinTime: new Date('11-Nov-2021'),
+        checkoutTime: new Date('12-Nov-2021'),
+        rating: 2.6
+      }
+    ]
+  }
 
   toggle() {
     this.hideRooms = !this.hideRooms;
+  }
+
+  selectRoom(room: RoomList){
+    console.log(room);
+    this.selectedRoom = room
   }
 }
