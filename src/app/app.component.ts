60efc392d7cc9573935e63dbdec1e5aee70aabdc
Debraj Deb <debrajdeb137@gmail.com>
2025-02-22 22:53:29 +0530

    dynamically rendered element using viewChild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ComponentFactoryResolver, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
+import { RoomsComponent } from './rooms/rooms.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
+
   title = 'hotelinventoryapp';
-  role = 'Admin'
+  role = 'Admin';
+  @ViewChild('user', {read: ViewContainerRef}) vcr!: ViewContainerRef;
+
+  constructor(private componentFactoryResolver: ComponentFactoryResolver){}
+
+  ngAfterViewInit(): void {
+    // in ng12 it give error
+    // const componentRef = this.vcr.createComponent(RoomsComponent);
+     // Create the component factory for RoomsComponent
+     const componentFactory = this.componentFactoryResolver.resolveComponentFactory(RoomsComponent);
+     const componentRef = this.vcr.createComponent(componentFactory);
+     componentRef.instance.numberOfRooms = 50;
+  }
 }
