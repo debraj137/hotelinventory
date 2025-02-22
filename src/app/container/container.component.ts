de9b46dae487745c70cf486a5ad6a3a1d4c28278
Debraj Deb <debrajdeb137@gmail.com>
2025-02-22 23:55:38 +0530

    learned ngAfterContentInit

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -1,13 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, Component, ContentChild, OnInit } from '@angular/core';
+import { EmployeeComponent } from '../employee/employee.component';
 
 @Component({
   selector: 'app-container',
   templateUrl: './container.component.html',
   styleUrls: ['./container.component.scss']
 })
-export class ContainerComponent implements OnInit {
-
+export class ContainerComponent implements OnInit, AfterContentInit {
+  @ContentChild(EmployeeComponent) employee!: EmployeeComponent
   constructor() { }
+  ngAfterContentInit(): void {
+    console.log(this.employee);
+    this.employee.empName = 'Cena'
+  }
 
   ngOnInit(): void {
   }
--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee.component.scss']
 })
 export class EmployeeComponent implements OnInit {
+  empName:string = 'John';
 
   constructor() { }
 
