1bc376611d31bf31429b82404c17d6ad4d546b8c
Debraj Deb <debrajdeb137@gmail.com>
2025-02-23 14:35:32 +0530

    learned @Host

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -1,14 +1,16 @@
-import { AfterContentChecked, AfterContentInit, Component, ContentChild, OnInit } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, Component, ContentChild, Host, OnInit } from '@angular/core';
 import { EmployeeComponent } from '../employee/employee.component';
+import { RoomsService } from '../rooms/services/rooms.service';
 
 @Component({
   selector: 'app-container',
   templateUrl: './container.component.html',
-  styleUrls: ['./container.component.scss']
+  styleUrls: ['./container.component.scss'],
+  providers: [RoomsService]
 })
 export class ContainerComponent implements OnInit, AfterContentInit {
   @ContentChild(EmployeeComponent) employee!: EmployeeComponent
-  constructor() { }
+  constructor(@Host() private roomsService: RoomsService) { }
   ngAfterContentInit(): void {
     console.log(this.employee);
     this.employee.empName = 'Cena'
