81f9a5bb7c1383b55c9a149a0265c6846de09816
Debraj Deb <debrajdeb137@gmail.com>
2025-03-01 23:58:50 +0530

    wild card route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 function initFactory(initService: InitService){
   return ()=>initService.init();
@@ -32,7 +33,8 @@ function initFactory(initService: InitService){
     HeaderComponent,
     ContainerComponent,
     EmployeeComponent,
-    NavComponent
+    NavComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
