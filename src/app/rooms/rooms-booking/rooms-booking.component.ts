61b1100ceb6657920ac2744f43fcdb492189210a
Debraj Deb <debrajdeb137@gmail.com>
2025-03-02 00:55:41 +0530

    activated route

--- src/app/rooms/rooms-booking/rooms-booking.component.ts
+++ src/app/rooms/rooms-booking/rooms-booking.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-rooms-booking',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rooms-booking.component.scss']
 })
 export class RoomsBookingComponent implements OnInit {
-
-  constructor() { }
+  id:number = 0;
+  id$: Observable<number> = this.router.params.pipe(
+    map(params=>params['roomId'])
+  );;
+  constructor(private router: ActivatedRoute) { }
 
   ngOnInit(): void {
+    // console.log(this.router.params.subscribe((data)=>{
+    //   console.log('activatedRoute data: ',data);
+    //   this.id = data['roomId']
+    // }))
+
+       console.log(this.router.paramMap.subscribe((params)=>{
+      params.get('roomId');
+    }))
+
+    //it will work for different view, i.e, it will not work in case of child route because data
+    //will not get updated
+    // this.id = this.router.snapshot.params['roomId'];
   }
 
 }
