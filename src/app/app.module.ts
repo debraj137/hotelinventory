0faaeb2fb5eeff51de3c8cc0becc34c26fcc3033
Debraj Deb <debrajdeb137@gmail.com>
2025-03-02 13:59:57 +0530

    custom validation in tdf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,6 +25,7 @@ import { RoomsAddComponent } from './rooms/rooms-add/rooms-add.component';
 import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 import { HoverDirective } from './hover.directive';
+import { EmailvalidatorDirective } from './emailvalidator/emailvalidator.directive';
 
 function initFactory(initService: InitService){
   return ()=>initService.init();
@@ -43,7 +44,8 @@ function initFactory(initService: InitService){
     RoomsBookingComponent,
     RoomsAddComponent,
     LoginComponent,
-    HoverDirective
+    HoverDirective,
+    EmailvalidatorDirective
   ],
   imports: [
     BrowserModule,
