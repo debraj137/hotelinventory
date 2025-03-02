617b3b2743c850e822fa388b6fa6f1f2f8bcabb7
Debraj Deb <debrajdeb137@gmail.com>
2025-03-03 00:04:03 +0530

    learned route guard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { LoggerService } from './logger.service';
 import { localStorageToken } from './localstorage.token';
 import { InitService } from './init.service';
 import { ConfigService } from './services/config.service';
+import { NavigationEnd, NavigationStart, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -21,11 +23,24 @@ export class AppComponent implements OnInit{
     @Optional() private loggerService: LoggerService,
     @Inject(localStorageToken) private localStorage: any,
     private initService: InitService,
-    private configService: ConfigService
+    private configService: ConfigService,
+    private router: Router
   ){
     console.log("this.initService.config: ",this.initService.config);
   }
   ngOnInit(): void {
+    // this.router.events.subscribe(event=>console.log(event));
+    this.router.events.pipe(
+      filter((event) => event instanceof NavigationStart)
+    ).subscribe((event) => {
+      console.log('Navigation Started');
+    });
+
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event) => {
+        console.log('Navigation Completed');
+      });
     this.name.nativeElement.innerText = 'Hilton Hotel';
     this.loggerService.log('AppComponent.ngOnInit()');
     this.localStorage.setItem('name','Hilton Hotel')
