b035fbc14ee4ac31b0658701b44f818c491cc11c
Debraj Deb <debrajdeb137@gmail.com>
2025-03-09 23:46:23 +0530

    resolve guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,6 +19,7 @@ const routes: Routes = [
   { path: 'booking/:roomId', loadChildren: () => import('./booking/booking.module').then(m => m.BookingModule),
     // canActivate: [LoginGuard]
    },
+  { path: 'comments', loadChildren: () => import('./comment/comment.module').then(m => m.CommentModule) },
   {path: '**', component: NotfoundComponent}
 ];
 
